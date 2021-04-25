13adb35684100e6d704af7c09765bf172a512dc5
Linda Gratzer <waltergehts@gmail.com>
2021-04-25 21:22:16 +0200

    Add db migration to create a tag table

---
+++ src/Backend/Database/Entities/Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Perry.Database.Entities
+{
+    public partial class Tag
+    {
+        public Tag()
+        {
+            RecipeTags = new HashSet<RecipeTag>();
+        }
+
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+
+        public virtual ICollection<RecipeTag> RecipeTags { get; set; }
+    }
+}
--- src/Backend/Database/Migrations/20210425191758_CreateTagTable.Designer.cs
+++ src/Backend/Database/Migrations/20210425191758_CreateTagTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Perry.Database.Entities;
 
 namespace Perry.Database.Migrations
 {
     [DbContext(typeof(RecipesContext))]
-    partial class RecipesContextModelSnapshot : ModelSnapshot
+    [Migration("20210425191758_CreateTagTable")]
+    partial class CreateTagTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
