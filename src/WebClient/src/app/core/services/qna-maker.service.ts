bd1b2722c58f1ee918c987dd0a8c17502b9d557d
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-06-27 12:25:54 +0200

    Pass question as parameter for backend query qna

--- src/WebClient/src/app/core/services/qna-maker.service.ts
+++ src/WebClient/src/app/core/services/qna-maker.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { APP_CONFIG } from 'src/app/core/core.module';
 import { AppConfig } from 'src/app/core/models';
+import { ChatMessage } from '../models/chat-message.model';
+import { ChatMessageService } from './chat-message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +13,34 @@ import { AppConfig } from 'src/app/core/models';
 export class QnAMakerService {
   private readonly route: string = `${this.config.apiBaseUrl}/query-answer`;
 
-  constructor(@Inject(APP_CONFIG) private config: AppConfig, private http: HttpClient) { }
+  constructor(@Inject(APP_CONFIG) private config: AppConfig, 
+  private http: HttpClient,
+  private chatService: ChatMessageService) { }
 
-  public queryAnswer(question: string): void {
-    const headers = new HttpHeaders().append('question', question);
-    var result = this.http.get<string>(this.route, {headers}).subscribe(data => { console.log(data)}); 
-    console.log(result);
+  returnObservable():Observable<string>{
+    return this.http.get(this.route , {responseType: 'text'});
+}
+
+  private queryAnswer(question: string) : Observable<string> {
+    let params = new HttpParams();
+    params = params.append('question', question);
+
+    let result = this.http.get(this.route, {params: params, responseType: 'text' } );
+    return result;
+  }
+
+  public loadAnswer(question: ChatMessage): void {
+    this.queryAnswer(question.message)
+    // .pipe(finalize(() => this.isLoading = false)) //load and stuff - call in drawer :D
+      .subscribe(
+        result => {
+          const message = new ChatMessage(result, false);
+          this.chatService.addMessage(message);
+        },
+        error => {
+          // this.isLoading = false;
+          const message = new ChatMessage("Something went wrong. Please try again later.", false);
+          this.chatService.addMessage(message);
+        });
   }
 }
