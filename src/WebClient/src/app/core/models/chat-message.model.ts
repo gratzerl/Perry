dc96d133b911f433bef99ad87301bc653510ab0b
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-06-27 12:28:38 +0200

    Adjust ChatMessage constructor
    Now set dateTime whenever a new chatMessage is created

--- src/WebClient/src/app/core/models/chat-message.model.ts
+++ src/WebClient/src/app/core/models/chat-message.model.ts
@@ -1,7 +1,16 @@
+import { formatDate } from '@angular/common';
+
 export class ChatMessage {
+  public message: string;
+  public isInput: boolean;
+  public dateTime: string;
+
   constructor(
-    public dateTime: string,
-    public isInput: boolean,
-    public message: string
-  ) {   }
+    message: string,
+    isInput: boolean
+  ) {
+    this.message = message;
+    this.isInput = isInput;
+    this.dateTime = formatDate(new Date(), 'dd-MM-yyyy H:mm:ss', 'de-DE');
+  }
 }
