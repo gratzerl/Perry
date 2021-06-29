a2831f199159a84d9f96c338090b09dbf054258a
jdmayer <32510229+jdmayer@users.noreply.github.com>
2021-06-29 07:05:05 +0200

    Set ChatMessage properties directly in constructor

--- src/WebClient/src/app/core/models/chat-message.model.ts
+++ src/WebClient/src/app/core/models/chat-message.model.ts
@@ -1,16 +1,12 @@
 import { formatDate } from '@angular/common';
 
 export class ChatMessage {
-  public message: string;
-  public isInput: boolean;
   public dateTime: string;
 
   constructor(
-    message: string,
-    isInput: boolean
+    public message: string,
+    public isInput: boolean
   ) {
-    this.message = message;
-    this.isInput = isInput;
     this.dateTime = formatDate(new Date(), 'dd-MM-yyyy H:mm:ss', 'de-DE');
   }
 }
